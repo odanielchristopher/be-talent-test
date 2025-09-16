ce944f68679788670f71c53bdbce76aca6fda602
daniel christopher <daniel.2005.christopher@gmail.com>
2025-09-16 12:13:43 -0300

    chore: define path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite';
+/* eslint-disable import/no-extraneous-dependencies */
+import path from 'node:path';
+
 import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@views': path.resolve(__dirname, 'src', 'views'),
+      '@app': path.resolve(__dirname, 'src', 'app'),
+    },
+  },
 });
