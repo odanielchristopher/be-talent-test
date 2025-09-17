143efcc32a17839ea38bf19544af8419aa1d6293
daniel christopher <daniel.2005.christopher@gmail.com>
2025-09-16 23:55:55 -0300

    feat: restructures service files creating a new datamappers folder in app folder

---
+++ src/app/services/employeesService.ts
@@ -0,0 +1,23 @@
+import {
+  EmployeeDataMapper,
+  employeeDataMapper,
+} from '@app/datamappers/employeeDataMapper';
+
+import { HttpClient } from './utils/HttpClient';
+
+class EmployeesService {
+  constructor(private readonly httpClient: HttpClient) {}
+
+  async getAll() {
+    const data =
+      await this.httpClient.get<EmployeeDataMapper.PersistenceData[]>(
+        '/employees',
+      );
+
+    return data.map(employeeDataMapper.toDomain);
+  }
+}
+
+export const employeesService = new EmployeesService(
+  new HttpClient('http://localhost:3001'),
+);
