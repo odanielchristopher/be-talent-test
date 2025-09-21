02dbb3ac3fc6bbdac49e7838160bc647559d7cd1
daniel christopher <daniel.2005.christopher@gmail.com>
2025-09-20 21:36:08 -0300

    refactor: apply custom interface pattern

--- src/views/components/Button/index.tsx
+++ src/views/components/Button/index.tsx
@@ -3,27 +3,19 @@ import { ComponentProps } from 'react';
 import { IStyledButton, Styled } from './styles';
 
 export interface IButtonProps extends ComponentProps<'button'> {
-  isLoading?: boolean;
   type: 'button' | 'submit' | 'reset';
 }
 
 export default function Button({
   type,
   disabled,
-  isLoading,
   children,
   onClick,
   ...props
 }: IButtonProps & IStyledButton) {
   return (
-    <Styled.Button
-      type={type}
-      disabled={disabled || isLoading}
-      onClick={onClick}
-      {...props}
-    >
-      {!isLoading && children}
-      {/* {isLoading && <Spinner size={16} />} */}
+    <Styled.Button type={type} disabled={disabled} onClick={onClick} {...props}>
+      {children}
     </Styled.Button>
   );
 }
